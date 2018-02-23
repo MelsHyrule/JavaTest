cee7eba4409c45563f77f25a31b1e901cceb04c8
Melody Seda <melodyseda@Melodys-MacBook-Pro.local>
2018-02-23 11:04:54 -0400

    Initial commit

---
+++ src/firstClass.java
@@ -0,0 +1,22 @@
+
+public class firstClass { //the only thing you start with when making a new project
+
+	public static void main(String[] arg) { //you add this in
+		//where the work beings
+		
+		//The classic hello world app but with a twist
+		System.out.println("Hello World");
+		System.out.print("Now this is on the next line");
+		System.out.println("and this is on the same line"); 
+		//ln after print is like adding a new line character after the string
+		
+		//now string variables
+		String cat, dog;
+		cat = "kitty"; dog = "doggo";
+		System.out.print("Was that a " + cat + " or a " + dog + " i saw???");
+		
+		//note from prof: Indentation here is simply for the programmer, same for enters and other stuff
+		
+	}
+	
+}
